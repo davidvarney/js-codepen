0484f739a615977a0ca044342d21a08ced8e0277
David Varney <davidvarney@gmail.com>
2019-05-01 23:01:47 -0400

    Adds first test methods.

--- calculator.js
+++ calculator.js
@@ -3,5 +3,42 @@ class Calculator {
     //
   }
 
-  //
+  /**
+   * [add description]
+   * @param {[type]} numOne [description]
+   * @param {[type]} numTwo [description]
+   */
+  add(numOne, numTwo) {
+    return numOne + numTwo;
+  }
+
+  /**
+   * [subtract description]
+   * @param  {[type]} numOne [description]
+   * @param  {[type]} numTwo [description]
+   * @return {[type]}        [description]
+   */
+  subtract(numOne, numTwo) {
+    return numOne - numTwo;
+  }
+
+  /**
+   * [multiply description]
+   * @param  {[type]} numOne [description]
+   * @param  {[type]} numTwo [description]
+   * @return {[type]}        [description]
+   */
+  multiply(numOne, numTwo) {
+    return numOne * numTwo;
+  }
+
+  /**
+   * [divide description]
+   * @param  {[type]} numOne [description]
+   * @param  {[type]} numTwo [description]
+   * @return {[type]}        [description]
+   */
+  divide(numOne, numTwo) {
+    return numOne / numTwo;
+  }
 }
